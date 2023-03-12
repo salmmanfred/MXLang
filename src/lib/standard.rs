cbdae1429aa7ae1c2e3bcbf993bdae2fea155dbd
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2023-03-12 18:59:17 +0100

    MXLA

--- src/lib/standard.rs
+++ src/lib/standard.rs
@@ -7,7 +7,7 @@ pub enum Standard{
 
 pub fn fetch_standard_lib(str: &str)->Standard{
     match str{
-        "std" => Standard::Lib(include_str!("./std/std.sm").to_string()),
+        "std" => Standard::Lib(include_str!("./std/std.MXLA").to_string()),
         _=>{
             Standard::None
         }
